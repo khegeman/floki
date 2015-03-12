21cf950adb143e869a832261b9cc85081c8a07af
Kyle Hegeman <kyle.hegeman@gmail.com>
2015-03-12 04:48:55 -0400

    use of boost iterators in the kary search implementation

--- floki/algorithms.hpp
+++ floki/algorithms.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-
 #include <boost/simd/include/pack.hpp>
 #include <boost/simd/include/functions/ffs.hpp>
 #include <boost/simd/include/functions/hmsb.hpp>
@@ -22,21 +21,20 @@ struct equal_to {
 struct greater_equal {
 
   template <class T, class U>
-  typename boost::simd::meta::as_logical<U>::type
-  operator()(U const &t0, T const &key) const {
-    typename boost::simd::meta::as_logical<U>::type ret;
-    ret = t0 >= key;
-    return ret;
+  typename boost::simd::meta::as_logical<T>::type
+  operator()(T const &t0, U const &key) const {
+    typedef typename boost::simd::meta::as_logical<T>::type result_type;
+    return result_type(t0 >= key);
   }
 };
 
-template <class T, class UnOp> const T *find_if(const T *begin, const T *end, UnOp f) {
+template <class T, class UnOp>
+const T *find_if(const T *begin, const T *end, UnOp f) {
   typedef boost::simd::native<T, BOOST_SIMD_DEFAULT_EXTENSION> vT;
 
   static const std::size_t N = vT::static_size;
 
-  auto end2 = begin + (std::distance(begin,end) / N)*N;
-
+  auto end2 = begin + (std::distance(begin, end) / N) * N;
 
   for (; begin != end2; begin += N) {
     auto mask = boost::simd::hmsb(f(boost::simd::load<vT>(begin)));
@@ -45,7 +43,7 @@ template <class T, class UnOp> const T *find_if(const T *begin, const T *end, Un
       break;
     }
   }
-  //epilogue
+  // epilogue
   if (begin == end2) {
     begin = std::find_if(begin, end, f);
   }
